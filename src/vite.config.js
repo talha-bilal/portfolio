a0a71c10449cc7ccff4a61be7cd9c4bf0437470c
talha-bilal <90046561+talha-bilal@users.noreply.github.com>
2025-10-22 16:44:35 +0500

    Update vite.config.js

--- src/vite.config.js
+++ src/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // Replace with your actual GitHub repo name
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio',
+  base: '/portfolio/',
 })
